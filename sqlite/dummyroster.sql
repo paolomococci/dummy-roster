0b2f1c58fe7c7b10460ccd75059d2743b5d82bfd
paolomococci <paolomococci@gmail.com>
2022-11-01 15:48:18 +0100

    modified:   sqlite/dummyroster.sql

--- sqlite/dummyroster.sql
+++ sqlite/dummyroster.sql
@@ -13,64 +13,64 @@ DROP TABLE IF EXISTS "Carrier";
 
 -- Customer table
 CREATE TABLE "Customer" (
-  "Id" char(5) NOT NULL,
-  "Name" varchar(40) NOT NULL,
+  "Id" char(8) NOT NULL,
+  "Name" varchar(32) NOT NULL,
   "Description" TEXT NULL,
   "Picture" BLOB NULL,
-  "Contact" varchar(40) NULL
+  "Contact" varchar(32) NULL
 );
 
 -- Category table
 CREATE TABLE "Category" (
-  "Id" char(5) NOT NULL,
-  "Name" varchar(40) NOT NULL,
+  "Id" char(8) NOT NULL,
+  "Name" varchar(32) NOT NULL,
   "Description" TEXT NULL,
   "Picture" BLOB NULL
 );
 
 -- Product table
 CREATE TABLE "Product" (
-  "Id" char(5) NOT NULL,
-  "Name" varchar(40) NOT NULL,
+  "Id" char(8) NOT NULL,
+  "Name" varchar(32) NOT NULL,
   "Description" TEXT NULL,
   "Picture" BLOB NULL
 );
 
 -- Supplier table
 CREATE TABLE "Supplier" (
-  "Id" char(5) NOT NULL,
-  "Name" varchar(40) NOT NULL,
+  "Id" char(8) NOT NULL,
+  "Name" varchar(32) NOT NULL,
   "Description" TEXT NULL,
   "Picture" BLOB NULL
 );
 
 -- Employee table
 CREATE TABLE "Employee" (
-  "Id" char(5) NOT NULL,
-  "Name" varchar(40) NOT NULL,
+  "Id" char(8) NOT NULL,
+  "Name" varchar(32) NOT NULL,
   "Description" TEXT NULL,
   "Picture" BLOB NULL
 );
 
 -- Form table
 CREATE TABLE "Form" (
-  "Id" char(5) NOT NULL,
-  "Name" varchar(40) NOT NULL,
+  "Id" char(8) NOT NULL,
+  "Name" varchar(32) NOT NULL,
   "Description" TEXT NULL
 );
 
 -- Invoice table
 CREATE TABLE "Invoice" (
-  "Id" char(5) NOT NULL,
-  "Name" varchar(40) NOT NULL,
+  "Id" char(8) NOT NULL,
+  "Name" varchar(32) NOT NULL,
   "Description" TEXT NULL,
   "Picture" BLOB NULL
 );
 
 -- Carrier table
 CREATE TABLE "Carrier" (
-  "Id" char(5) NOT NULL,
-  "Name" varchar(40) NOT NULL,
+  "Id" char(8) NOT NULL,
+  "Name" varchar(32) NOT NULL,
   "Description" TEXT NULL,
   "Picture" BLOB NULL
 );
