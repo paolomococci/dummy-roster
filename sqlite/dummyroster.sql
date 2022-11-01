2c72034053d1661b38d7921944aa7f7526e1c5b0
paolomococci <paolomococci@gmail.com>
2022-11-01 21:25:32 +0100

    modified:   sqlite/dummyroster.sql

--- sqlite/dummyroster.sql
+++ sqlite/dummyroster.sql
@@ -3,13 +3,13 @@
 -- drop tables if exists
 
 DROP TABLE IF EXISTS "Customer";
-DROP TABLE IF EXISTS "Category";
-DROP TABLE IF EXISTS "Product";
 DROP TABLE IF EXISTS "Supplier";
 DROP TABLE IF EXISTS "Employee";
+DROP TABLE IF EXISTS "Carrier";
+DROP TABLE IF EXISTS "Category";
+DROP TABLE IF EXISTS "Product";
 DROP TABLE IF EXISTS "Form";
 DROP TABLE IF EXISTS "Invoice";
-DROP TABLE IF EXISTS "Carrier";
 
 -- Customer table
 CREATE TABLE "Customer" (
