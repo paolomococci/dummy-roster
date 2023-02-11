b0322092ad0872ec17f0c7c29004b73c17d37016
paolomococci <paolomococci@gmail.com>
2023-02-11 15:12:21 +0100

    modified:   mariadb/dummy-roster.rc0/dummy_roster_rc0.sql

--- mariadb/dummy-roster.rc0/dummy_roster_rc0.sql
+++ mariadb/dummy-roster.rc0/dummy_roster_rc0.sql
@@ -253,7 +253,8 @@ CREATE TABLE `Product` (
   `Belonging` varchar(8) DEFAULT NULL,
   `Picture` blob DEFAULT NULL,
   `QuantityPerUnit` varchar(24) DEFAULT NULL,
-  `UnitPrice` NUMERIC NULL CHECK (`UnitPrice`>0),
+  `UnitPrice` NUMERIC NULL CHECK (`UnitPrice`>=0),
+  `UnitsInStock` SMALLINT NULL CHECK (`UnitsInStock`>=0),
   PRIMARY KEY (`Id`),
   KEY `IDX_ProductName` (`Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
