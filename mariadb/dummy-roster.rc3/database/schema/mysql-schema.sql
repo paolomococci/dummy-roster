000bbe44128a39a1a462cd9f26d0ba8c26943e66
paolomococci <paolomococci@gmail.com>
2023-02-15 22:15:50 +0100

    modified:   mariadb/dummy-roster.rc3/database/schema/mysql-schema.sql

--- mariadb/dummy-roster.rc3/database/schema/mysql-schema.sql
+++ mariadb/dummy-roster.rc3/database/schema/mysql-schema.sql
@@ -9,9 +9,20 @@ DROP TABLE IF EXISTS `addresses`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `addresses` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  `civic` varchar(16) DEFAULT NULL,
+  `city` varchar(16) DEFAULT NULL,
+  `district` varchar(16) DEFAULT NULL,
+  `postcode` varchar(8) DEFAULT NULL,
+  `country` varchar(16) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `idx_address_name` (`name`),
+  KEY `idx_address_city` (`city`),
+  KEY `idx_address_district` (`district`),
+  KEY `idx_address_postcode` (`postcode`),
+  KEY `idx_address_country` (`country`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `carriers`;
