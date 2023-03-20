1c39e72cba63fd8604d1ec0f0d7643ccaae90b26
paolomococci <paolomococci@gmail.com>
2023-03-20 13:13:54 +0100

    modified:   notebooks/notebook_db_rc0/note2.sql

--- notebooks/notebook_db_rc0/note2.sql
+++ notebooks/notebook_db_rc0/note2.sql
@@ -1,31 +1,31 @@
 CREATE TABLE IF NOT EXISTS `customers` (
-
+    `customer_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `payments` (
-    
+    `payment_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `products` (
-    
+    `product_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `product_lines` (
-    
+    `product_line_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `orders` (
-    
+    `order_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `order_details` (
-    
+    `order_detail_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `employees` (
-    
+    `employee_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `offices` (
-    
+    `office_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
