633f3a28670788f42d0cfee8f6644c6a4345ef2e
paolomococci <paolomococci@gmail.com>
2023-03-20 14:29:02 +0100

    modified:   notebooks/notebook_db_rc0/note2.sql

--- notebooks/notebook_db_rc0/note2.sql
+++ notebooks/notebook_db_rc0/note2.sql
@@ -1,5 +1,19 @@
+USE DATABASE `notebook_db_rc0`;
+
 CREATE TABLE IF NOT EXISTS `customers` (
     `customer_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `customer_name` VARCHAR(64) NOT NULL,
+    `contact_name` VARCHAR(64) DEFAULT NULL,
+    `phone` VARCHAR(64) DEFAULT NULL,
+    `address` VARCHAR(64) DEFAULT NULL,
+    `city` VARCHAR(64) DEFAULT NULL,
+    `state` VARCHAR(64) DEFAULT NULL,
+    `postal_code` VARCHAR(64) DEFAULT NULL,
+    `country` VARCHAR(64) DEFAULT NULL,
+    `sales` INT(11) DEFAULT NULL,
+    `credit_limit` DECIMAL(10,2) DEFAULT NULL,
+    KEY `sales` (`sales`),
+    CONSTRAIN `fk_customer_employee` FOREIGN KEY (`sales`) REFERENCES `employees` (`employee_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `payments` (
