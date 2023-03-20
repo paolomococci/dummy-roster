e19cd4ad87083d330dca7b85feb1d0cecfc4f685
paolomococci <paolomococci@gmail.com>
2023-03-20 09:29:33 +0100

    modified:   notebooks/notebook_db_rc0/note1.sql

--- notebooks/notebook_db_rc0/note1.sql
+++ notebooks/notebook_db_rc0/note1.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS `note_one`(
     `description` CHAR(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-SELECT * FROM `note_one`;
+SELECT * FROM `note_one`
+WHERE `description` IS NOT NULL;
 
 INSERT INTO `note_one` (`title`, `description`)
 VALUES ('someone', 'some description of someone');
@@ -15,7 +16,7 @@ TRUNCATE `note_one`;
 INSERT INTO `note_one` (`title`, `description`)
 VALUES 
     ('title 1', 'description 1'),
-    ('title 2', 'description 2'),
+    ('title 2', NULL),
     ('title 3', 'description 3'),
-    ('title 4', 'description 4'),
+    ('title 4', NULL),
     ('title 5', 'description 5');
