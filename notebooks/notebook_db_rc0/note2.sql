ce8ae66ac21be83be2989d1735788f5e3c75a5e1
paolomococci <paolomococci@gmail.com>
2023-03-20 17:41:56 +0100

    modified:   notebooks/notebook_db_rc0/note2.sql

---
+++ notebooks/notebook_db_rc0/note2.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS `addresses`;
+CREATE TEMPORARY TABLE `addresses` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  `civic` varchar(16) DEFAULT NULL,
+  `city` varchar(16) DEFAULT NULL,
+  `district` varchar(16) DEFAULT NULL,
+  `postcode` varchar(8) DEFAULT NULL,
+  `country` varchar(16) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_address_name` (`name`),
+  KEY `idx_address_city` (`city`),
+  KEY `idx_address_district` (`district`),
+  KEY `idx_address_postcode` (`postcode`),
+  KEY `idx_address_country` (`country`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
