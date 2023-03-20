ce792f6def0fe21a2f28e63241527830713e0dc7
paolomococci <paolomococci@gmail.com>
2023-03-20 13:07:14 +0100

    modified:   notebooks/notebook_db_rc0/note2.sql

--- notebooks/notebook_db_rc0/note2.sql
+++ notebooks/notebook_db_rc0/note2.sql
@@ -1,3 +1,7 @@
 CREATE TABLE IF NOT EXISTS `customers` (
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `products` (
     
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
