ccc954a0d5306cab7e9592c8dd47de1c304262d7
paolomococci <paolomococci@gmail.com>
2023-03-21 07:17:16 +0100

    modified:   notebooks/notebook_db_rc0/note2.sql

--- notebooks/notebook_db_rc0/note2.sql
+++ notebooks/notebook_db_rc0/note2.sql
@@ -21,7 +21,7 @@ CREATE TABLE `addresses` (
   KEY `idx_address_district` (`district`),
   KEY `idx_address_postcode` (`postcode`),
   KEY `idx_address_country` (`country`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `addresses`;
 
@@ -35,7 +35,7 @@ CREATE TABLE `credentials` (
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `idx_credential_email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `credentials`;
 
@@ -61,7 +61,7 @@ CREATE TABLE `employees` (
   KEY `idx_employee_name` (`name`),
   CONSTRAINT `fk_employee_address` FOREIGN KEY (`loc`) REFERENCES `addresses` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_employee_credential` FOREIGN KEY (`ref`) REFERENCES `credentials` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `employees`;
 
@@ -86,7 +86,7 @@ CREATE TABLE `carriers` (
   CONSTRAINT `fk_carrier_address` FOREIGN KEY (`loc`) REFERENCES `addresses` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_carrier_credential` FOREIGN KEY (`ref`) REFERENCES `credentials` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_carrier_employee` FOREIGN KEY (`contact`) REFERENCES `employees` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `carriers`;
 
@@ -111,7 +111,7 @@ CREATE TABLE `customers` (
   CONSTRAINT `fk_customer_address` FOREIGN KEY (`loc`) REFERENCES `addresses` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_customer_credential` FOREIGN KEY (`ref`) REFERENCES `credentials` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_customer_employee` FOREIGN KEY (`contact`) REFERENCES `employees` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `customers`;
 
@@ -125,7 +125,7 @@ CREATE TABLE `forms` (
   `formDate` DATETIME DEFAULT NULL,
   `required_date` DATETIME DEFAULT NULL,
   `promised_date` DATETIME DEFAULT NULL,
-  `shipping_cost` DECIMAL(10,2) DEFAULT 0 CHECK (`shipping_cost` >= 0),
+  `shipping_cost` DECIMAL(10, 2) DEFAULT 0 CHECK (`shipping_cost` >= 0),
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
@@ -138,7 +138,7 @@ CREATE TABLE `forms` (
   CONSTRAINT `fk_form_carrier` FOREIGN KEY (`carrier_id`) REFERENCES `carriers` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_form_customer` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_form_employee` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `forms`;
 
@@ -159,7 +159,7 @@ CREATE TABLE `suppliers` (
   CONSTRAINT `fk_supplier_address` FOREIGN KEY (`loc`) REFERENCES `addresses` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_supplier_credential` FOREIGN KEY (`ref`) REFERENCES `credentials` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_supplier_employee` FOREIGN KEY (`contact`) REFERENCES `employees` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `suppliers`;
 
@@ -174,7 +174,7 @@ CREATE TABLE `categories` (
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `idx_category_name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `categories`;
 
@@ -188,7 +188,7 @@ CREATE TABLE `products` (
   `category_id` BIGINT(20) UNSIGNED NULL,
   `supplier_id` BIGINT(20) UNSIGNED NULL,
   `quantityPerUnit` VARCHAR(24) DEFAULT NULL,
-  `unit_price` DECIMAL(10,2) DEFAULT 0 CHECK (`unit_price` >= 0),
+  `unit_price` DECIMAL(10, 2) DEFAULT 0 CHECK (`unit_price` >= 0),
   `unitsIn_stock` SMALLINT(6) DEFAULT 0 CHECK (`unitsIn_stock` >= 0),
   `units_on_order` SMALLINT(6) DEFAULT 0 CHECK (`units_on_order` >= 0),
   `reorder_level` SMALLINT(6) DEFAULT 0 CHECK (`reorder_level` >= 0),
@@ -203,7 +203,7 @@ CREATE TABLE `products` (
   KEY `idx_product_supplied_by` (`supplier_id`),
   CONSTRAINT `fk_product_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_product_supplier` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `products`;
 
@@ -214,9 +214,12 @@ CREATE TABLE `invoices` (
   `product_id` BIGINT(20) UNSIGNED NULL,
   `issuing_date` DATETIME DEFAULT NULL,
   `note` TEXT DEFAULT NULL,
-  `unit_price` DECIMAL(10,2) NOT NULL DEFAULT 0 CHECK (`unit_price` >= 0),
+  `unit_price` DECIMAL(10, 2) NOT NULL DEFAULT 0 CHECK (`unit_price` >= 0),
   `quantity` SMALLINT(6) NOT NULL DEFAULT 1 CHECK (`quantity` > 0),
-  `price_cut` DECIMAL(10,2) NOT NULL DEFAULT 0 CHECK (`price_cut` >= 0 and `price_cut` < 0.3),
+  `price_cut` DECIMAL(10, 2) NOT NULL DEFAULT 0 CHECK (
+    `price_cut` >= 0
+    and `price_cut` < 0.3
+  ),
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
@@ -225,6 +228,6 @@ CREATE TABLE `invoices` (
   KEY `idx_invoice_id` (`id`),
   CONSTRAINT `fk_form_details` FOREIGN KEY (`form_id`) REFERENCES `forms` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_product_details` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `invoices`;
