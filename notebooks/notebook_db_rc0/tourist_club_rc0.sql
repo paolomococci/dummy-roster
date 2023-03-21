c837f06f75fe5ed19f39efccf5eb8a1b7606796e
paolomococci <paolomococci@gmail.com>
2023-03-21 15:06:33 +0100

    modified:   notebooks/notebook_db_rc0/tourist_club_rc0.sql

--- notebooks/notebook_db_rc0/tourist_club_rc0.sql
+++ notebooks/notebook_db_rc0/tourist_club_rc0.sql
@@ -1,5 +1,20 @@
+/*markdown
+# ϰοσμοπολίτης (cosmopolitan)
+Notebook dedicated to a hypothetical cosmopolitan tourism club.
+*/
+
 SHOW DATABASES;
 
 USE `notebook_db_rc0`;
 
 SHOW TABLES;
+
+/*markdown
+### Driver's ID type table.
+*/
+
+DROP TABLE IF EXISTS `driver_id_types`;
+CREATE TABLE `driver_id_types` (
+    `id` INT(11) DEFAULT NULL,
+    `type` VARCHAR(255) DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
