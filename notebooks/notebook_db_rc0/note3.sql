9db7372959eaf2e9410bf65474f0954ffbf6246c
paolomococci <paolomococci@gmail.com>
2023-03-21 07:39:39 +0100

    modified:   notebooks/notebook_db_rc0/note3.sql

--- notebooks/notebook_db_rc0/note3.sql
+++ notebooks/notebook_db_rc0/note3.sql
@@ -3,3 +3,16 @@ SHOW TABLES;
 SET FOREIGN_KEY_CHECKS = 0;
 SET @tables = NULL;
 SET GROUP_CONCAT_MAX_LEN = 32768;
+
+SELECT GROUP_CONCAT('`', table_schema, '`.`', table_name, '`') INTO @tables
+FROM information_schema.tables
+WHERE table_schema = (
+        SELECT DATABASE()
+    );
+SELECT IFNULL(@tables, '') INTO @tables;
+SET @tables = CONCAT('DROP TABLE IF EXISTS ', @tables);
+PREPARE cleaning_statement
+FROM @tables;
+EXECUTE cleaning_statement;
+DEALLOCATE PREPARE cleaning_statement;
+SET FOREIGN_KEY_CHECKS = 1;
