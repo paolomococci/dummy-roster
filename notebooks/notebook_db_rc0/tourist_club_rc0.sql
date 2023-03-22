03458c570f24e3fcee0437c9d1ba75d86fed02a5
paolomococci <paolomococci@gmail.com>
2023-03-22 12:49:22 +0100

    modified:   notebooks/notebook_db_rc0/tourist_club_rc0.sql

--- notebooks/notebook_db_rc0/tourist_club_rc0.sql
+++ notebooks/notebook_db_rc0/tourist_club_rc0.sql
@@ -53,3 +53,24 @@ CREATE TABLE `brands` (
     `id` INT(11) DEFAULT NULL,
     `name` VARCHAR(30) DEFAULT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+LOCK TABLES `brands` WRITE;
+INSERT INTO `brands` (`id`, `name`)
+VALUES (
+    1, ''
+), (
+    2, ''
+), (
+    3, ''
+), (
+    4, ''
+), (
+    5, ''
+), (
+    6, ''
+), (
+    7, ''
+), (
+    8, ''
+);
+UNLOCK TABLES;
