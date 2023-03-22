da3d068d5bd16132e07c523562acfe3114ede4bd
paolomococci <paolomococci@gmail.com>
2023-03-22 13:08:56 +0100

    modified:   notebooks/notebook_db_rc0/tourist_club_rc0.sql

--- notebooks/notebook_db_rc0/tourist_club_rc0.sql
+++ notebooks/notebook_db_rc0/tourist_club_rc0.sql
@@ -210,3 +210,37 @@ VALUES (
 UNLOCK TABLES;
 
 SELECT * FROM `vehicle_use_types`;
+
+
+/*markdown
+### vehicle_variant_types table
+*/
+
+DROP TABLE IF EXISTS `vehicle_variant_types`;
+CREATE TABLE `vehicle_variant_types` (
+    `id` INT(11) DEFAULT NULL,
+    `name` VARCHAR(30) DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+LOCK TABLES `vehicle_variant_types` WRITE;
+INSERT INTO `vehicle_variant_types` (`id`, `name`)
+VALUES (
+    1, ''
+), (
+    2, ''
+), (
+    3, ''
+), (
+    4, ''
+), (
+    5, ''
+), (
+    6, ''
+), (
+    7, ''
+), (
+    8, ''
+);
+UNLOCK TABLES;
+
+SELECT * FROM `vehicle_variant_types`;
