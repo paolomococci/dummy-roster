4c1b919e72a615e54350dcdeee43e2dd2c46ddda
paolomococci <paolomococci@gmail.com>
2023-03-23 09:49:59 +0100

    new file:   notebooks/notebook_db_rc1/clean_all_tables_of_notebook_db_rc1.sql

---
+++ notebooks/notebook_db_rc1/clean_all_tables_of_notebook_db_rc1.sql
@@ -0,0 +1,20 @@
+USE `notebook_db_rc1`;
+SHOW TABLES;
+SET FOREIGN_KEY_CHECKS = 0;
+SET @tables = NULL;
+SET GROUP_CONCAT_MAX_LEN = 32768;
+
+SELECT GROUP_CONCAT('`', table_schema, '`.`', table_name, '`') INTO @tables
+FROM information_schema.tables
+WHERE table_schema = (
+        SELECT DATABASE()
+    );
+SELECT IFNULL(@tables, '') INTO @tables;
+SET @tables = CONCAT('DROP TABLE IF EXISTS ', @tables);
+PREPARE cleaning_statement
+FROM @tables;
+EXECUTE cleaning_statement;
+DEALLOCATE PREPARE cleaning_statement;
+SET FOREIGN_KEY_CHECKS = 1;
+
+SHOW TABLES;
