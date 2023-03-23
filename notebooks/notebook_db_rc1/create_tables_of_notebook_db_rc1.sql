7ca9e5c5a9f3c11611719518c19a485aeb02a797
paolomococci <paolomococci@gmail.com>
2023-03-23 15:52:44 +0100

    modified:   notebooks/notebook_db_rc1/create_tables_of_notebook_db_rc1.sql

--- notebooks/notebook_db_rc1/create_tables_of_notebook_db_rc1.sql
+++ notebooks/notebook_db_rc1/create_tables_of_notebook_db_rc1.sql
@@ -433,3 +433,12 @@ CREATE TABLE `movie_content` (
 /*markdown
 ## I create the pivot table movie_player
 */
+
+DROP TABLE IF EXISTS `movie_player`;
+CREATE TABLE `movie_player` (
+  `movie_id` BIGINT(20) UNSIGNED NOT NULL,
+  `player_id` BIGINT(20) UNSIGNED NOT NULL,
+  PRIMARY KEY (`movie_id`,`player_id`),
+  CONSTRAINT `movie_player_to_movie_fk` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`),
+  CONSTRAINT `movie_player_to_player_fk` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
