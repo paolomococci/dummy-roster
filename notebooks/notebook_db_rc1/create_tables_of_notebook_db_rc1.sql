6d7862b84fd4c5b3f34ae385a8372d6eb722659f
paolomococci <paolomococci@gmail.com>
2023-03-23 11:26:58 +0100

    modified:   notebooks/notebook_db_rc1/create_tables_of_notebook_db_rc1.sql

--- notebooks/notebook_db_rc1/create_tables_of_notebook_db_rc1.sql
+++ notebooks/notebook_db_rc1/create_tables_of_notebook_db_rc1.sql
@@ -1,3 +1,18 @@
 /*markdown
 # create the tables of a hypothetical movie store
 */
+
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE `address` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `civic` VARCHAR(10) NOT NULL,
+  `phone` VARCHAR(25) DEFAULT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `type` VARCHAR(255) DEFAULT NULL,
+  `district_fk` BIGINT(20) UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `address_to_district_fk` (`district_fk`),
+  CONSTRAINT `address_to_district_fk` FOREIGN KEY (`district_fk`) REFERENCES `district` (`id`) ON DELETE SET NULL
+)ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
