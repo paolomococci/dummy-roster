c9735c3e2cb0c844f4389995512d121ca6995e58
paolomococci <paolomococci@gmail.com>
2023-03-24 06:27:33 +0100

    modified:   notebooks/notebook_db_rc0/admin.sql

--- notebooks/notebook_db_rc0/admin.sql
+++ notebooks/notebook_db_rc0/admin.sql
@@ -1,3 +1,12 @@
 /*markdown
 # Simple scripts that can be used to administer an RDBMS
 */
+
+/*markdown
+## Create Database
+*/
+
+CREATE DATABASE notebook_db_rcn;
+GRANT ALL PRIVILEGES ON notebook_db_rcn.* TO 'username'@'localhost';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'username'@'localhost';
