381a06ef9ac6bf492e3a1939ba77eac2128cb3e1
paolomococci <paolomococci@gmail.com>
2023-03-24 16:21:34 +0100

    modified:   notebooks/notebook_db_rc3/cosmopolitan_tourist_club.sql

--- notebooks/notebook_db_rc3/cosmopolitan_tourist_club.sql
+++ notebooks/notebook_db_rc3/cosmopolitan_tourist_club.sql
@@ -445,7 +445,8 @@ CREATE TABLE `registrations` (
     `year` VARCHAR(30) DEFAULT NULL,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_un` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `registrations`;
