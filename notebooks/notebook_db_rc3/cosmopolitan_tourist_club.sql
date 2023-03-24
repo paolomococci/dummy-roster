ea13f0b7a378e77694ba7141ef78048537582032
paolomococci <paolomococci@gmail.com>
2023-03-24 16:00:23 +0100

    modified:   notebooks/notebook_db_rc3/cosmopolitan_tourist_club.sql

--- notebooks/notebook_db_rc3/cosmopolitan_tourist_club.sql
+++ notebooks/notebook_db_rc3/cosmopolitan_tourist_club.sql
@@ -401,6 +401,8 @@ DROP TABLE IF EXISTS `vehicle_variant_types`;
 CREATE TABLE `vehicle_variant_types` (
     `id` INT(11) DEFAULT NULL,
     `variant` VARCHAR(30) DEFAULT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
