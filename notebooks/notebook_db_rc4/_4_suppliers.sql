acbfcaf16c5b15bc565327d4e4883870be8a098b
paolomococci <paolomococci@gmail.com>
2023-03-25 15:39:47 +0100

    modified:   notebooks/notebook_db_rc4/_4_suppliers.sql

--- notebooks/notebook_db_rc4/_4_suppliers.sql
+++ notebooks/notebook_db_rc4/_4_suppliers.sql
@@ -8,3 +8,19 @@
 
 USE `notebook_db_rc4`;
 SHOW TABLES;
+
+/*markdown
+## I create the table suppliers
+*/
+
+DROP TABLE IF EXISTS `suppliers`;
+CREATE TABLE `suppliers` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(30) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `description` VARCHAR(200) DEFAULT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `email_uc` UNIQUE (`email`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
