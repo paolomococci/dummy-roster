9c1b08248c56857110d0dd99ef3f905ea4100888
paolomococci <paolomococci@gmail.com>
2023-03-25 21:56:47 +0100

    modified:   notebooks/notebook_db_rc4/_6_employee_office.sql

--- notebooks/notebook_db_rc4/_6_employee_office.sql
+++ notebooks/notebook_db_rc4/_6_employee_office.sql
@@ -19,8 +19,8 @@ CREATE TABLE `employee_office` (
   `employee_id` BIGINT(20) UNSIGNED NOT NULL,
   `office_id` BIGINT(20) UNSIGNED NOT NULL,
   PRIMARY KEY (`employee_id`,`office_id`),
-  CONSTRAINT `employee_office_to_employees_fk` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`),
-  CONSTRAINT `employee_office_to_offices_fk` FOREIGN KEY (`office_id`) REFERENCES `offices` (`id`)
+  CONSTRAINT `employee_office_to_employees_fk` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `employee_office_to_offices_fk` FOREIGN KEY (`office_id`) REFERENCES `offices` (`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `employee_office`;
