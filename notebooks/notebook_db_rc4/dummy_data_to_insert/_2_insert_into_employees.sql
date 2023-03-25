bb7b8d27d464e4b6dfbeb35b2ce553fc9b7724df
paolomococci <paolomococci@gmail.com>
2023-03-25 21:32:53 +0100

    modified:   notebooks/notebook_db_rc4/dummy_data_to_insert/_2_insert_into_employees.sql

--- notebooks/notebook_db_rc4/dummy_data_to_insert/_2_insert_into_employees.sql
+++ notebooks/notebook_db_rc4/dummy_data_to_insert/_2_insert_into_employees.sql
@@ -13,3 +13,8 @@ SHOW TABLES;
 /*markdown
 ## I insert some data in the table employees
 */
+
+INSERT INTO `employees` (`name`, `email`, `hired`, `salary`)
+VALUES (
+        'John Doe', 'john.doe@example.local', '2021-10-12', 1350.00
+    );
