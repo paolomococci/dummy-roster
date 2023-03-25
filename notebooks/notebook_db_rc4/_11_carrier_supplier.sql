0d6380922f42978a5007b6cb5e31c308b3e9e810
paolomococci <paolomococci@gmail.com>
2023-03-25 17:04:10 +0100

    modified:   notebooks/notebook_db_rc4/_11_carrier_supplier.sql

--- notebooks/notebook_db_rc4/_11_carrier_supplier.sql
+++ notebooks/notebook_db_rc4/_11_carrier_supplier.sql
@@ -13,3 +13,12 @@ SHOW TABLES;
 /*markdown
 ## I create the pivot table carrier_supplier
 */
+
+DROP TABLE IF EXISTS `carrier_supplier`;
+CREATE TABLE `carrier_supplier` (
+  `carrier_id` BIGINT(20) UNSIGNED NOT NULL,
+  `supplier_id` BIGINT(20) UNSIGNED NOT NULL,
+  PRIMARY KEY (`carrier_id`,`supplier_id`),
+  CONSTRAINT `carrier_supplier_to_carriers_fk` FOREIGN KEY (`carrier_id`) REFERENCES `carriers` (`id`),
+  CONSTRAINT `carrier_supplier_to_suppliers_fk` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
