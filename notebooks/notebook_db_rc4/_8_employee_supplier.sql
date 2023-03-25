c0171f7d5ac0e5ba684e931a53cbb9dac8a8bbe1
paolomococci <paolomococci@gmail.com>
2023-03-25 17:03:41 +0100

    modified:   notebooks/notebook_db_rc4/_8_employee_supplier.sql

--- notebooks/notebook_db_rc4/_8_employee_supplier.sql
+++ notebooks/notebook_db_rc4/_8_employee_supplier.sql
@@ -13,3 +13,12 @@ SHOW TABLES;
 /*markdown
 ## I create the pivot table employee_supplier
 */
+
+DROP TABLE IF EXISTS `employee_supplier`;
+CREATE TABLE `employee_supplier` (
+  `employee_id` BIGINT(20) UNSIGNED NOT NULL,
+  `supplier_id` BIGINT(20) UNSIGNED NOT NULL,
+  PRIMARY KEY (`employee_id`,`supplier_id`),
+  CONSTRAINT `employee_supplier_to_employees_fk` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`),
+  CONSTRAINT `employee_supplier_to_suppliers_fk` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
