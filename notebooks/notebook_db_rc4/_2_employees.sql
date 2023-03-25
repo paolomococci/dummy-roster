36d733031fa5eedc34f696e79eb6bbc63e48c890
paolomococci <paolomococci@gmail.com>
2023-03-25 15:31:40 +0100

    modified:   notebooks/notebook_db_rc4/_2_employees.sql

--- notebooks/notebook_db_rc4/_2_employees.sql
+++ notebooks/notebook_db_rc4/_2_employees.sql
@@ -26,3 +26,5 @@ CREATE TABLE `employees` (
   PRIMARY KEY (`id`),
   CONSTRAINT `email_uc` UNIQUE (`email`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+DESCRIBE `employees`;
