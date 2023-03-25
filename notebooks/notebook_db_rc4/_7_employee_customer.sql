9ab711a6298b91880d7552cb34ab7dc609494d4b
paolomococci <paolomococci@gmail.com>
2023-03-25 21:58:19 +0100

    modified:   notebooks/notebook_db_rc4/_7_employee_customer.sql

--- notebooks/notebook_db_rc4/_7_employee_customer.sql
+++ notebooks/notebook_db_rc4/_7_employee_customer.sql
@@ -19,8 +19,8 @@ CREATE TABLE `employee_customer` (
   `employee_id` BIGINT(20) UNSIGNED NOT NULL,
   `customer_id` BIGINT(20) UNSIGNED NOT NULL,
   PRIMARY KEY (`employee_id`,`customer_id`),
-  CONSTRAINT `employee_customer_to_employees_fk` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`),
-  CONSTRAINT `employee_customer_to_customers_fk` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
+  CONSTRAINT `employee_customer_to_employees_fk` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `employee_customer_to_customers_fk` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 DESCRIBE `employee_customer`;
