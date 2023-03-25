1836c74239cd6eca1a5eff947c4f1874ce6883b7
paolomococci <paolomococci@gmail.com>
2023-03-25 17:10:56 +0100

    modified:   notebooks/notebook_db_rc4/_7_employee_customer.sql

--- notebooks/notebook_db_rc4/_7_employee_customer.sql
+++ notebooks/notebook_db_rc4/_7_employee_customer.sql
@@ -22,3 +22,5 @@ CREATE TABLE `employee_customer` (
   CONSTRAINT `employee_customer_to_employees_fk` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`),
   CONSTRAINT `employee_customer_to_customers_fk` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+DESCRIBE `employee_customer`;
