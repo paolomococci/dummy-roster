8f0f13c789e2861fc9e81d5c2477222f9599ea03
paolomococci <paolomococci@gmail.com>
2023-03-25 15:23:44 +0100

    modified:   notebooks/notebook_db_rc4/_2_employees.sql

--- notebooks/notebook_db_rc4/_2_employees.sql
+++ notebooks/notebook_db_rc4/_2_employees.sql
@@ -8,3 +8,21 @@
 
 USE `notebook_db_rc4`;
 SHOW TABLES;
+
+/*markdown
+## I create the table employees
+*/
+
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE `employees` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(30) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `hired` DATE DEFAULT NULL,
+  `salary` DECIMAL(10,2) DEFAULT NULL,
+  `description` VARCHAR(200) DEFAULT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `email_uc` UNIQUE (`email`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
