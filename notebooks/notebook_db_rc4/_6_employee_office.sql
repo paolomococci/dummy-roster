5a4283ca753986a600055d72032020f27b9626fa
paolomococci <paolomococci@gmail.com>
2023-03-25 17:03:21 +0100

    modified:   notebooks/notebook_db_rc4/_6_employee_office.sql

--- notebooks/notebook_db_rc4/_6_employee_office.sql
+++ notebooks/notebook_db_rc4/_6_employee_office.sql
@@ -13,3 +13,12 @@ SHOW TABLES;
 /*markdown
 ## I create the pivot table employee_office
 */
+
+DROP TABLE IF EXISTS `employee_office`;
+CREATE TABLE `employee_office` (
+  `employee_id` BIGINT(20) UNSIGNED NOT NULL,
+  `office_id` BIGINT(20) UNSIGNED NOT NULL,
+  PRIMARY KEY (`employee_id`,`office_id`),
+  CONSTRAINT `employee_office_to_employees_fk` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`),
+  CONSTRAINT `employee_office_to_offices_fk` FOREIGN KEY (`office_id`) REFERENCES `offices` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
