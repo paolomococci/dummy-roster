b67c40157789d3eb8802d31f6aa26fe3b98aaaee
paolomococci <paolomococci@gmail.com>
2023-03-26 16:41:38 +0200

    modified:   notebooks/notebook_db_rc4/dummy_data_to_insert/_6_insert_into_employee_office.sql

--- notebooks/notebook_db_rc4/dummy_data_to_insert/_6_insert_into_employee_office.sql
+++ notebooks/notebook_db_rc4/dummy_data_to_insert/_6_insert_into_employee_office.sql
@@ -9,3 +9,15 @@
 USE `notebook_db_rc4`;
 
 SHOW TABLES;
+
+/*markdown
+### I select all records in table offices
+*/
+
+SELECT * FROM `offices` ORDER BY `email`;
+
+/*markdown
+### I select all records in table employees
+*/
+
+SELECT * FROM `employees` ORDER BY `email`;
